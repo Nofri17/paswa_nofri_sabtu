84bb501ff5dbda63b5222a13f215e2d0367412d5
unknown <nofri.wilis@gmail.com>
2022-11-10 22:19:47 +0700

    registrasi

--- login.php
+++ login.php
@@ -96,10 +96,7 @@
                                     <?php } ?>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a href="registrasi.php" class="btn btn-secondary btn-user btn-block">Registrasi</a>
                                     </div>
                                 </div>
                             </div>
